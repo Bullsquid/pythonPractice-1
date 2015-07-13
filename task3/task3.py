bc1c6f5620a34993f628dd4bb2a3a8b14d6dba1b
Andrew Boyko <andrew.boyko@filmon.com>
2015-07-13 16:22:14 +0300

    task3: code style

--- task3/task3.py
+++ task3/task3.py
@@ -1,10 +1,13 @@
-import sys , random
-def  compute(n):
-    i = 0; s = 0
-    while i  <= n:
-        s +=  random.random ()
+import sys, random
+
+
+def compute(n):
+    i = 0
+    s = 0
+    while i <= n:
+        s += random.random()
         i += 1
     return s/n
 
-n = int(sys.argv [1])
-print  "average  of %d random  numbers  is %g" % (n, compute(n))
+n = int(sys.argv[1])
+print "average  of %d random  numbers  is %g" % (n, compute(n))
